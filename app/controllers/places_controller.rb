f4a900fe459168cfaeb771f2aaf22b0df8f018ae
Gamal Al-Hetari <gn62192@gmail.com>
2017-08-03 19:31:21 +0000

    handle form

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -6,4 +6,15 @@ class PlacesController < ApplicationController
     def new
         @place = Place.new
     end
+    
+    def create
+        Place.create(place_params)
+        redirect_to root_path
+    end
+    
+    private
+    
+    def place_params
+        params.require(:place).permit(:name, :description, :address)
+    end
 end
