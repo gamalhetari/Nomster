6588733b354414566bcf39c5c086ffe0fe5594a4
Gamal Al-Hetari <gn62192@gmail.com>
2017-08-04 22:40:36 +0000

    style sign,forgot,reset

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -166,4 +166,5 @@ SimpleForm.setup do |config|
 
   # Defines which i18n scope will be used in Simple Form.
   # config.i18n_scope = 'simple_form'
+  config.label_text = lambda { |label, required| "#{label}" }
 end
