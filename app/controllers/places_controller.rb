e94baa6109f6ab19739ae95977159aec8b13bf73
Gamal Al-Hetari <gn62192@gmail.com>
2017-08-05 18:23:02 +0000

    place detail

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,7 +2,7 @@ class PlacesController < ApplicationController
     before_action :authenticate_user!, only: [:new, :create]
     
     def index
-        @places=Place.order(:name).page params[:page]
+        @places = Place.order(:name).page params[:page]
     end
     
     def new
@@ -12,6 +12,10 @@ class PlacesController < ApplicationController
     def create
         current_user.places.create(place_params)
         redirect_to root_path
+    end
+    
+    def show
+        @place = Place.find(params[:id])
     end
     
     private
--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -166,5 +166,4 @@ SimpleForm.setup do |config|
 
   # Defines which i18n scope will be used in Simple Form.
   # config.i18n_scope = 'simple_form'
-  config.label_text = lambda { |label, required| "#{label}" }
 end
