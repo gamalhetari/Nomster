337ac7df3070e0ae5b834d7731751acd8501cbf8
Gamal Al-Hetari <gn62192@gmail.com>
2017-08-06 19:29:47 +0000

    validation

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -11,7 +11,12 @@ class PlacesController < ApplicationController
     
     def create
         current_user.places.create(place_params)
-        redirect_to root_path
+        
+        if @place.valid?
+            redirect_to root_path
+        else
+            render :new, status: :unprocessable_entity
+        end
     end
     
     def show
@@ -34,7 +39,12 @@ class PlacesController < ApplicationController
         end
         
         @place.update_attributes(place_params)
-        redirect_to root_path
+        
+        if @place.valid?
+            redirect_to root_path
+        else
+            render :edit, status: :unprocessable_entity
+        end
     end
     
     def destroy
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,5 @@
 class Place < ApplicationRecord
     paginates_per 5
     belongs_to :user
+    validates :name, presence: true
 end
