ea5c53a3ac15f4f36e245772d39c875ffb75de60
Gamal Al-Hetari <gn62192@gmail.com>
2017-08-06 19:32:25 +0000

    validation

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -10,7 +10,7 @@ class PlacesController < ApplicationController
     end
     
     def create
-        current_user.places.create(place_params)
+        @place = current_user.places.create(place_params)
         
         if @place.valid?
             redirect_to root_path
