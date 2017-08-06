120f9222462ccc2c026f0eab4ebc1e822ccf256d
Gamal Al-Hetari <gn62192@gmail.com>
2017-08-06 17:17:50 +0000

    permission

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-    before_action :authenticate_user!, only: [:new, :create]
+    before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
     
     def index
         @places = Place.order(:name).page params[:page]
@@ -20,16 +20,30 @@ class PlacesController < ApplicationController
     
     def edit
         @place = Place.find(params[:id])
+        
+        if @place.user != current_user
+            return render text: 'Not allowed', status: :forbidden
+        end
     end
     
     def update
         @place = Place.find(params[:id])
+        
+        if @place.user != current_user
+            return render text: 'Not allowed', status: :forbidden
+        end
+        
         @place.update_attributes(place_params)
         redirect_to root_path
     end
     
     def destroy
         @place = Place.find(params[:id])
+        
+        if @place.user != current_user
+            return render text: 'Not allowed', status: :forbidden
+        end
+        
         @place.destroy
         redirect_to root_path
     end
