e8d894e1f614a9a31deb4c49c8ff090341e02f69
Gamal Al-Hetari <gn62192@gmail.com>
2017-08-06 19:44:43 +0000

    more validation

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,8 +1,7 @@
 class Place < ApplicationRecord
     paginates_per 5
     belongs_to :user
-    validates :name, presence: true
-    validates :name, length: { minimum: 4 }
+    validates :name, presence: true, length: { minimum: 4 }
     validates :address, presence: true
     validates :description, presence: true
 end
