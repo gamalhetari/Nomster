c7ae5af3613c3ea3371c5e8a3b81ec2bea41da07
Gamal Al-Hetari <gn62192@gmail.com>
2017-08-09 14:28:31 +0000

    comment database

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,6 +1,7 @@
 class Place < ApplicationRecord
     paginates_per 5
     belongs_to :user
+    has_many :comments
     geocoded_by :address
     after_validation :geocode
     validates :name, presence: true, length: { minimum: 4 }
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170808165530) do
+ActiveRecord::Schema.define(version: 20170809141956) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "message"
+    t.string   "rating"
+    t.integer  "user_id"
+    t.integer  "place_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["place_id"], name: "index_comments_on_place_id", using: :btree
+    t.index ["user_id", "place_id"], name: "index_comments_on_user_id_and_place_id", using: :btree
+  end
 
   create_table "places", force: :cascade do |t|
     t.string   "name"
