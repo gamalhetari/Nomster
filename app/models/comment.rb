26fdd766d9ab8c44a6e01a665f21c998f8dbdf7f
Gamal Al-Hetari <gn62192@gmail.com>
2017-08-11 16:11:15 +0000

    rating dropdown

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,16 @@
 class Comment < ApplicationRecord
     belongs_to :user
     belongs_to :place
+    
+    RATINGS = {
+        'one star': '1_star',
+        'two star': '2_stars',
+        'three star': '3_stars',
+        'four star': '4_stars',
+        'five star': '5_stars'
+    }
+    
+    def humanized_rating
+        RATINGS.invert[self.rating]
+    end
 end
