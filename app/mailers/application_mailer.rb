e18644dce64d7266dcee18193ab363b2aa2a9c51
Gamal Al-Hetari <gn62192@gmail.com>
2017-08-14 21:12:37 +0000

    email basics

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,9 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'no-reply@nomsterapp.com'
   layout 'mailer'
+  
+  def comment_added
+    mail(to: "gn62192@gmail.com",
+         subject: "A comment has been added to your place")
+  end
 end
