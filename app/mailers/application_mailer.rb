5e2d972bd6e5c22512ed1c83279b0f730dd3c31e
Gamal Al-Hetari <gn62192@gmail.com>
2017-08-14 22:15:30 +0000

    adjusting mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,8 +2,10 @@ class ApplicationMailer < ActionMailer::Base
   default from: 'no-reply@nomsterapp.com'
   layout 'mailer'
   
-  def comment_added
-    mail(to: "gn62192@gmail.com",
-         subject: "A comment has been added to your place")
+  def comment_added(comment)
+    @place = comment.place
+    @place_owner = @place.user
+    mail(to: @place_owner.email,
+         subject: "A comment has been added to #{@place.name}")
   end
 end
