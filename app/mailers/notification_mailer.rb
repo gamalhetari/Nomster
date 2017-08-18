222e1ae8d05a98d142c5ab33e2b818e603ed9a3e
Gamal Al-Hetari <gn62192@gmail.com>
2017-08-18 08:14:46 +0000

    mailing on production

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,8 +1,2 @@
 class NotificationMailer < ApplicationMailer
-    def comment_added(comment)
-    @place = comment.place
-    @place_owner = @place.user
-    mail(to: @place_owner.email,
-         subject: "A comment has been added to #{@place.name}")
-    end
 end
